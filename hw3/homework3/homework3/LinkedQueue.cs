fe4eb41e3b1408a3ffbf4567991b977c4845a407
klyu521 <klyu16@wou.edu>
2018-10-14 23:34:54 -0700

    hw

---
+++ hw3/homework3/homework3/LinkedQueue.cs
@@ -0,0 +1,76 @@
+﻿
+
+public class LinkedQueue<T> : IQueueInterface<T>
+{
+	private Node<T> front;
+	private Node<T> rear;
+
+	public LinkedQueue()
+	{
+		front = null;
+		rear = null;
+	}
+
+	public virtual T Push(T element)
+	{
+		if (object.Equals(element, default(T)))
+
+        {
+			throw new System.NullReferenceException();
+		}
+
+		if (Empty)
+		{
+			Node<T> tmp = new Node<T>(element, null);
+			rear = front = tmp;
+		}
+		else
+		{
+			// General case
+			Node<T> tmp = new Node<T>(element, null);
+			rear.next = tmp;
+			rear = tmp;
+		}
+		return element;
+	}
+
+	public virtual T Pop()
+	{
+		T tmp = default;
+		if (Empty)
+		{
+			throw new QueueUnderflowException("The queue was empty when pop was invoked.");
+		}
+		else if (front == rear)
+		{ // one item in queue
+			tmp = front.data;
+			front = null;
+			rear = null;
+		}
+		else
+		{
+			// General case
+			tmp = front.data;
+			front = front.next;
+		}
+
+		return tmp;
+	}
+
+	public virtual bool Empty
+	{
+		get
+		{
+			if (front == null && rear == null)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+}
+
---
+++ hw3/homework3/homework3/QueueUnderflowException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// A custom unchecked exception to represent situations where 
+/// an illegal operation was performed on an empty queue.
+/// </summary>
+public class QueueUnderflowException : Exception
+{
+  public QueueUnderflowException() : base()
+  {
+  }
+
+  public QueueUnderflowException(string message) : base(message)
+  {
+  }
+}
