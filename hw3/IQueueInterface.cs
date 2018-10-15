5d992f1bb20ccc3ebd3dfad4aeb2a162838df455
klyu521 <klyu16@wou.edu>
2018-10-14 20:13:32 -0700

    hw3

---
+++ hw3/IQueueInterface.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// A FIFO queue interface.  
+/// linked queue.
+/// </summary>
+public interface IQueueInterface<T>
+{
+	/// <summary>
+	/// Add an element to the rear of the queue
+	/// </summary>
+	/// <returns> the element that was enqueued </returns>
+	T Push(T element);
+
+	/// <summary>
+	/// Remove and return the front element.
+	/// </summary>
+	/// <exception cref="Thrown"> if the queue is empty </exception>
+//JAVA TO C# CONVERTER WARNING: Method 'throws' clauses are not available in .NET:
+//ORIGINAL LINE: T pop() throws QueueUnderflowException;
+	T Pop();
+
+	/// <summary>
+	/// Test if the queue is empty
+	/// </summary>
+	/// <returns> true if the queue is empty; otherwise false </returns>
+	bool Empty {get;}
+}
+
+
+
+
---
+++ hw3/Node.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// Singly linked node class. </summary>
+
+public class Node<T>
+{
+	public T data;
+	public Node<T> next;
+
+	public Node(T data, Node<T> next)
+	{
+		this.data = data;
+		this.next = next;
+	}
+}
