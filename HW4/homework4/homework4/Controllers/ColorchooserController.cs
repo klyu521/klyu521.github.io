74e4288cbf8d552d2a5e07c7adea0609886478f1
klyu521 <klyu16@wou.edu>
2018-10-21 23:35:33 -0700

    hw4 program

---
+++ HW4/homework4/homework4/Controllers/ColorchooserController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace homework4.Controllers
+{
+    public class ColorchooserController : Controller
+    {
+        // GET: Colorchooser
+        
+        public ActionResult Mix()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Mix(string firstColor, string secondColor)
+        {
+           
+            // Convert colors to Color object
+            Color firstRgb = ColorTranslator.FromHtml(firstColor);
+            Color secondRgb = ColorTranslator.FromHtml(secondColor);
+
+            // Define red, grenn, blue to calcuate 
+            int red = LimitValue(firstRgb.R, secondRgb.R);
+            int green =LimitValue(firstRgb.G, secondRgb.G);
+            int blue = LimitValue(firstRgb.B, secondRgb.B);
+
+            // Use values to calculate the result
+            Color newColor = Color.FromArgb(red, green, blue);
+             string Result = ColorTranslator.ToHtml(newColor);
+
+            // Use ViewBag with three colors
+            ViewBag.ColorOne = "background:" + firstColor;
+            ViewBag.ColorTwo = "background:" + secondColor;
+            ViewBag.ColorThree = "background:" + Result;
+
+            return View();
+        }
+
+        // The max value is 255
+        private int LimitValue(int first, int second)
+        {
+           
+            const int max = 255;
+
+            // Add both colors and return that value if it's less than 255
+            int value = first + second;
+            if (value < max) return first + second;
+            return max;
+        }
+    }
+}
---
+++ HW4/homework4/homework4/Controllers/ConverterController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace homework4.Controllers
+{
+    public class ConverterController : Controller
+    {
+        // GET: Converter
+        [HttpGet]
+       
+        public ActionResult Converter()
+        { 
+            string num = Request.QueryString["num"];
+            string type = Request.QueryString["type"];
+
+            ViewBag.num = num;
+            ViewBag.type = type;
+            if (!String.IsNullOrEmpty(num))
+            {
+                if (type.StartsWith("a"))
+                {
+                    ViewBag.a = Convert.ToDouble(num);
+                    ViewBag.b = ViewBag.a * 1609344;
+                    ViewBag.c = ViewBag.a * 160934.4;
+                    ViewBag.d = ViewBag.a * 1609.34;
+                    ViewBag.e = ViewBag.a * 1.60934;
+                    return View("Result");
+                }
+
+                else if (type[0] == 'b')
+                {
+                    ViewBag.b = Convert.ToDouble(num);
+                    ViewBag.a = ViewBag.d * 0.00062;
+                    ViewBag.c = ViewBag.b * 0.1;
+                    ViewBag.d = ViewBag.b * 0.001;
+                    ViewBag.e = ViewBag.b * 0.000001;
+                    return View("Result");
+                }
+
+                else if (type[0] == 'c')
+                {
+                    ViewBag.c = Convert.ToDouble(num);
+                    ViewBag.a = ViewBag.e * 0.6214;
+                    ViewBag.b = ViewBag.c * 10;
+                    ViewBag.d = ViewBag.c * 0.01;
+                    ViewBag.e = ViewBag.c * 0.00001;
+                    return View("Result");
+                }
+
+                else if (type[0] == 'd')
+                {
+                    ViewBag.d = Convert.ToDouble(num);
+                    ViewBag.a = ViewBag.e * 0.6214;
+                    ViewBag.b = ViewBag.d * 1000;
+                    ViewBag.c = ViewBag.d * 100;
+                    ViewBag.e = ViewBag.d * 0.001;
+                    return View("Result");
+                }
+
+                else if (type[0] == 'e')
+                {
+                    ViewBag.e = Convert.ToDouble(num);
+                    ViewBag.a = ViewBag.e * 0.6214;
+                    ViewBag.b = ViewBag.e * 1000000;
+                    ViewBag.c = ViewBag.e * 100000;
+                    ViewBag.d = ViewBag.e * 1000;
+                    return View("Result");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage("Invalid input");
+                    return View();
+                }
+
+            }
+
+
+            return View();
+        }
+    }
+}
